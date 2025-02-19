5d2c6b4bf32299a29de60ca7fed1e3b80a4267b7
monk0 <liuhuifeng.bj@gmail.com>
2025-02-19 22:11:24 +0800

    Add microsoft-office to installation script for common tools

--- install_apps.sh
+++ install_apps.sh
@@ -2,7 +2,7 @@
 
 # 安装常用工具
 # 1.检查并安装 google-chrome wechat tencent-meeting
-for package in google-chrome wechat tencent-meeting; do
+for package in google-chrome wechat tencent-meeting microsoft-office; do
     if ! brew list --cask $package &>/dev/null; then
         echo "$package is not installed. Installing $package..."
         brew install $package
